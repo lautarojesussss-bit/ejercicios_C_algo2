b96ba7fef6ebd0d5d893739762b62a352dbeb240
lautarojesussss-bit <lautarojesussss@gmail.com>
2026-03-11 08:08:20 -0300

    Todavía no sé si se puede utilizar string.h, sino tendría que editar eso de usar strlen, simplemente revisaría todo el tiempo si estoy en el caracter nulo o no

--- ejercicio_split.c
+++ ejercicio_split.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "split.h"
 #include <stdlib.h>
-#include <string.h>
+
 
 //"Hola;1;2;3;mundo"
 // ["Hola", "1","2","3","mundo"]
@@ -9,15 +8,24 @@
 //creo que a grandes rasgos ya está la función, me falta pensarla un toque más y escribir el error_memoria
 
 
-struct vector {
-        unsigned int cantidad;
-        char **palabras;
-};
+/*
+*PRE:
+*POST:
+*/
+void vector_destruir(struct vector *v)
+{
+        for (int i = 0; i < v->cantidad; i++)
+                free(v->palabras[i]);
+        
+        free(v);
+}
 
-//no puedo hacer strcpy así que tengo que copiar caracter por caracter básicamente
-//tengo que usar memoria dinámica para esto, o sea tengo que reservar un bloque para cada palabra, y según si se agranda o no le tengo que hacer realloc
-//PRE: texto debe ser un string
-//POST: se devuelve un struct palabas que en su campo palabras tiene el vector con las palabras de texto, luego de separarlas considerando el caracter separador, el campo cantidad del struct palabras devuelto es la cantidad de strings del vector palabras, devuelve un struct palabras_separadas con NULL en el campo palabras y 0 en el campo cantidad si hubo un error en el proceso
+
+/*
+*PRE: texto debe ser un string
+*POST: se devuelve un puntero al struct con el vector que tiene las palabras, ya separadas,
+*y su tamaño en el campo cantidad, si falla algo se devuelve NULL 
+*/ 
 struct vector *split(char *texto, char separador)
 {
         struct vector *resultado = malloc(sizeof(struct vector));
@@ -26,7 +34,7 @@ struct vector *split(char *texto, char separador)
                 return NULL;
         
         resultado->cantidad = 0;
-        int cant_letras_aux = 1;
+        int cant_letras_aux = 0;
 
         for (int i = 0; i < strlen(texto); i++) {
                 if (texto[i] == separador || i == 0) {
@@ -58,29 +66,12 @@ struct vector *split(char *texto, char separador)
         return resultado;
 
 error_memoria: 
-        for (int j = 0; j < resultado->cantidad; j++) 
-                free(resultado->palabras[j]);
-
-        free(resultado);
+        vector_destruir(resultado);
         return NULL;
 }
 
 
-void vector_destruir(struct vector *v)
-{
-    
-}
-
-// cómo puedo testeear esto ahora? sí o sí necesito valgrind gcc y gdb creo
+// cómo puedo testeear esto ahora? sí o sí necesito valgrind gcc y gdb creo, o sea crear la maquina virtual y correrlo ahí
 
 
 
-
-
-
-
-
-
-
-
-//hasta acá puede lleger mi función como máximo, 50 lineas
