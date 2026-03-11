2fd5bc8547f2db42c27bd9a5f85d0631740d482a
lautarojesussss-bit <lautarojesussss@gmail.com>
2026-03-11 05:55:19 -0300

    Segunda versión, ahora me di cuenta que quieren que sean en realidad dos funciones y la función split debe devolver un puntero a un struct vector en memoria dinámica.

--- ejercicio_split.c
+++ ejercicio_split.c
@@ -9,7 +9,7 @@
 //creo que a grandes rasgos ya está la función, me falta pensarla un toque más y escribir el error_memoria
 
 
-struct palabras_separadas {
+struct vector {
         unsigned int cantidad;
         char **palabras;
 };
@@ -18,10 +18,14 @@ struct palabras_separadas {
 //tengo que usar memoria dinámica para esto, o sea tengo que reservar un bloque para cada palabra, y según si se agranda o no le tengo que hacer realloc
 //PRE: texto debe ser un string
 //POST: se devuelve un struct palabas que en su campo palabras tiene el vector con las palabras de texto, luego de separarlas considerando el caracter separador, el campo cantidad del struct palabras devuelto es la cantidad de strings del vector palabras, devuelve un struct palabras_separadas con NULL en el campo palabras y 0 en el campo cantidad si hubo un error en el proceso
-struct palabras_separadas split(char *texto, char separador)
+struct vector *split(char *texto, char separador)
 {
-        struct palabras_separadas resultado;
-        resultado.cantidad = 0;
+        struct vector *resultado = malloc(sizeof(struct vector));
+
+        if (!resultado)
+                return NULL;
+        
+        resultado->cantidad = 0;
         int cant_letras_aux = 1;
 
         for (int i = 0; i < strlen(texto); i++) {
@@ -29,21 +33,21 @@ struct palabras_separadas split(char *texto, char separador)
                         char* palabra_aux = malloc(sizeof(char));
 
                         if (palabra_aux != NULL) {
-                                resultado.palabras[resultado.cantidad] = palabra_aux;
-                                resultado.palabras[resultado.cantidad][0] = '\0';
+                                resultado->palabras[resultado->cantidad] = palabra_aux;
+                                resultado->palabras[resultado->cantidad][0] = '\0';
                                 cant_letras_aux = 1;
-                                resultado.cantidad++;
+                                resultado->cantidad++;
                         } else {
                                 goto error_memoria;    
                         }
                 } else {
                         char* palabra_actualizada = realloc(
-                            resultado.palabras[resultado.cantidad - 1], (cant_letras_aux + 1)*(sizeof(char)));
+                            resultado->palabras[resultado->cantidad - 1], (cant_letras_aux + 1)*(sizeof(char)));
 
                         if (palabra_actualizada != NULL) {
-                                resultado.palabras[resultado.cantidad - 1] = palabra_actualizada;
-                                resultado.palabras[resultado.cantidad - 1][cant_letras_aux - 1] = texto[i];
-                                resultado.palabras[resultado.cantidad - 1][cant_letras_aux] = '\0'; 
+                                resultado->palabras[resultado->cantidad - 1] = palabra_actualizada;
+                                resultado->palabras[resultado->cantidad - 1][cant_letras_aux - 1] = texto[i];
+                                resultado->palabras[resultado->cantidad - 1][cant_letras_aux] = '\0'; 
                                 cant_letras_aux++;
                         } else {
                                 goto error_memoria;
@@ -54,17 +58,18 @@ struct palabras_separadas split(char *texto, char separador)
         return resultado;
 
 error_memoria: 
-        for (int j = 0; j < resultado.cantidad; j++) 
-                free(resultado.palabras[j]);
+        for (int j = 0; j < resultado->cantidad; j++) 
+                free(resultado->palabras[j]);
 
-        resultado.cantidad = 0;
-        resultado.palabras = NULL;
-
-        return resultado;
+        free(resultado);
+        return NULL;
 }
 
 
-
+void vector_destruir(struct vector *v)
+{
+    
+}
 
 // cómo puedo testeear esto ahora? sí o sí necesito valgrind gcc y gdb creo
 
