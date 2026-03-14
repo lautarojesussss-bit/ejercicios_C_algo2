46a93838a1ac4424b65e336d6075d8571ab29b94
lautarojesussss-bit <lautarojesussss@gmail.com>
2026-03-14 07:42:25 -0300

    Me había olvidado de destruir el vector en la prueba.c, por eso me quedaba la memoria reservada en el heap al pedo

--- prueba.c
+++ prueba.c
@@ -7,14 +7,17 @@
 
 //"Hola;1;2;3;mundo"
 // ["Hola", "1","2","3","mundo"]
+//la H no me la devolvió parece
 
 int main()
 {
         char *texto = TEXTO_PRUEBA;
-        struct vector *v= split(texto, separador_prueba);
+        struct vector *v = split(texto, separador_prueba);
 
         for (int i = 0; i < v->cantidad; i++)
             printf("%s\n",v->palabras[i]);
         
+        vector_destruir(v);
+
         return 0;
 }
