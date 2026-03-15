c6ebaaede9ab75ac5b7cf7ab5e25b42b193fc5ac
lautarojesussss-bit <lautarojesussss@gmail.com>
2026-03-14 22:51:50 -0300

    Ya hice algunas cosas para automatizar las pruebas, y funciona toavía

--- prueba.c
+++ prueba.c
@@ -1,23 +1,39 @@
 #include "split.h"
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
-#define TEXTO_PRUEBA "Hola;1;2;3;mundo"
+#define MAX_ENTRADA 200
+#define SALIDA "exit\n"
+#define AVISO_INICIAL "Ingrese un string para dividr (%i caracteres como máximo), usando ; como separador, O escriba exit para salir\n"
 #define separador_prueba ';'
 
-//"Hola;1;2;3;mundo"
-// ["Hola", "1","2","3","mundo"]
-//la H no me la devolvió parece
+
+void limpiar_entrada(char* entrada)
+{
+        size_t len = strlen(entrada);
+
+        if (entrada[len-1]== '\n')
+                entrada[len-1] = '\0';
+}
 
 int main()
 {
-        char *texto = TEXTO_PRUEBA;
-        struct vector *v = split(texto, separador_prueba);
+        char leido[MAX_ENTRADA];
+        printf(AVISO_INICIAL, MAX_ENTRADA-1);
+    
+        while(fgets(leido, MAX_ENTRADA, stdin) != NULL && strcmp(leido, SALIDA) != 0){
+                limpiar_entrada(leido);
 
-        for (int i = 0; i < v->cantidad; i++)
-            printf("%s\n",v->palabras[i]);
-        
-        vector_destruir(v);
+                struct vector *v = split(leido, separador_prueba);
+                printf("Las palabras son:\n");
+
+                for (size_t i = 0; i < v->cantidad; i++)
+                        printf("%s\n",v->palabras[i]);
+
+                vector_destruir(v);
+                printf(AVISO_INICIAL, MAX_ENTRADA-1);        
+        }
 
         return 0;
 }
